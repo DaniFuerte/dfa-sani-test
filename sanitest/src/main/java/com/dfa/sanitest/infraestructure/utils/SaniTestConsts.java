9bc367dd8aacd687af49f10106b52dd06a05f630
DaniFuerte <dani.fuerte.alvarez@gmail.com>
2023-03-12 13:25:06 +0100

    #7 configuracion de swagger

--- sanitest/src/main/java/com/dfa/sanitest/infraestructure/utils/SaniTestConsts.java
+++ sanitest/src/main/java/com/dfa/sanitest/infraestructure/utils/SaniTestConsts.java
@@ -1,8 +1,19 @@
 package com.dfa.sanitest.infraestructure.utils;
 
+import org.apache.logging.log4j.util.Strings;
+
 public class SaniTestConsts {
 
-    public static final String  NOT_FOUND        = "NOT FOUND";
-    public static final String  DATE_FORMAT      = "yyyy-MM-dd HH:mm:ss";
-    
+    private static final String SPACE_STRING        = " ";
+
+    public static final String  NOT_FOUND           = "NOT FOUND";
+    public static final String  DATE_FORMAT         = "yyyy-MM-dd HH:mm:ss";
+    public static final String  LOCALHOST           = "http://localhost:8081/";
+
+    public static final String  VERSION             = "1.0";
+    public static final String  API_SERV            = "Service API";
+    public static final String  API_REST            = "API REST";
+    public static final String  APP_NAME            = "Sanitas Test";
+    public static final String  FULL_APP_NAME       = APP_NAME + SPACE_STRING + API_SERV;
+    public static final String  DESC_APP_NAME       = APP_NAME + SPACE_STRING + API_REST;
 }
